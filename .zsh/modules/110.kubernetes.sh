fc2d8e0e5fd0be0fb06e2d14c4d57b0bb6634415
R. Aidan Campbell <aidan.campbell@gmail.com>
2020-09-27 11:16:40 -0700

    lazy load kubectl completion.  first execution of kc will load completion then execute the command

--- .zsh/modules/110.kubernetes.sh
+++ .zsh/modules/110.kubernetes.sh
@@ -2,9 +2,14 @@ if [ -z "$(command -v kubectl)" ]; then
   return
 fi
 
-alias kc=kubectl
-
 if [ -n "$(echo $ZSH_NAME)" ]; then
-  source <(kubectl completion zsh)
-  complete -F __start_kubectl kc
+  function kc() {
+    if ! type __start_kubectl >/dev/null 2>&1; then
+      source <(command kubectl completion zsh)
+      complete -F __start_kubectl kc
+    fi
+    kubectl "$@"
+  }
+else
+  alias kc=kubectl
 fi
