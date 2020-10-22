b1ec27d1ac3b4e6bc81e4c37d3eae6ca4ce93492
R. Aidan Campbell <aidan.campbell@gmail.com>
2020-10-22 14:32:08 -0700

    add some increasingly arcane kubernetes aliases.  mostly for future reference.

--- .zsh/modules/110.kubernetes.sh
+++ .zsh/modules/110.kubernetes.sh
@@ -13,3 +13,39 @@ if [ -n "$(echo $ZSH_NAME)" ]; then
 else
   alias kc=kubectl
 fi
+
+alias king='kubectl get ingress'
+#alias watchpo=''
+alias kcdebug='kubectl run -i --rm --tty $(whoami)-debug --image=alpine --restart=Never -- sh -c "apk --no-cache add curl ; sh" '
+
+# scrapes the raw metrics endpoint
+# flattens and ignores multiple containers per pod
+# converts resource usage from nanocores and kilobytes of memory into millicores and megabytes
+# sorts by most CPU usage
+function kcresource() {
+    kubectl get --raw /apis/metrics.k8s.io/v1beta1/pods\
+     | jq '[.items [] | {podName: .metadata.name, podNamespace: .metadata.namespace, c0CPUMilli: .containers[0].usage.cpu, c0mem: .containers[0].usage.memory}] | [.[] | .c0CPUMilli = (.c0CPUMilli | rtrimstr("n") | tonumber | ./1000000)] | [.[] | .c0MemMB = (.c0mem | rtrimstr("Ki") | tonumber | ./1024)]  | sort_by(.c0CPUMilli) | reverse| del(.[].c0mem)'
+}
+
+# see kcresource, but sorted by memory usage instead
+function kcresource_memsort() {
+    kcresource | jq 'sort_by(.c0MemMB) | reverse'
+}
+
+# thanks, https://talkcloudlytome.com/raw-kubernetes-metrics-with-kubectl/
+function kc_node_resource() {
+  kubectl get --raw /apis/metrics.k8s.io/v1beta1/nodes | jq '[.items [] | {nodeName: .metadata.name, nodeCpu: .usage.cpu, nodeMemory: .usage.memory}]'
+}
+
+# thanks, https://talkcloudlytome.com/raw-kubernetes-metrics-with-kubectl/
+function kc_container_resource() {
+  kubectl get --raw /apis/metrics.k8s.io/v1beta1/pods | jq '[.items [] | {podName: .metadata.name, podNamespace: .metadata.namespace, containers: [{name: .containers[].name, cpu: .containers[].usage.cpu, memory: .containers[].usage.memory}]}]'
+}
+
+function kc_container_total_millicores() {
+  kubectl get --raw /apis/metrics.k8s.io/v1beta1/pods | jq '[.items[].containers[].usage.cpu | rtrimstr("n") | tonumber] | add | . / 1000000'
+}
+
+function kc_nodes_total_millicores() {
+  kubectl get --raw /apis/metrics.k8s.io/v1beta1/nodes | jq '[.items[].usage.cpu | rtrimstr("n") | tonumber] | add | . / 1000000'
+}
