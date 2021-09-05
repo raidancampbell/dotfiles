837c9dc903ba02778ad3fd6a20bfc825da035aa1
R. Aidan Campbell <aidan.campbell@gmail.com>
2021-09-05 09:13:58 -0700

    add function to set some macOS defaults

--- .zsh/modules/200.macos.func.sh
+++ .zsh/modules/200.macos.func.sh
@@ -26,3 +26,24 @@ function proxyoff() {
   echo Do not forget to close the SSH session!
   networksetup -setsocksfirewallproxystate "Wi-Fi" off
 }
+
+# this is only intended to be run once per OS instance, it's a collection of OS default changes
+# thanks https://macos-defaults.com/#%F0%9F%92%BB-list-of-commands
+function set_defaults() {
+# don't warn on file extension changes
+  defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "true"
+# show file extensions
+  defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+# spaces should not reorder themselves based on most recently used
+  defaults write com.apple.dock "mru-spaces" -bool "false"
+# show the full directory path at the bottom of the current Finder window
+  defaults write com.apple.finder "ShowPathbar" -bool "true"
+# Finder should show  contents in list view (as opposed to icons, column, or gallery)
+  defaults write com.apple.Finder "FXPreferredViewStyle" -String "Nlsv"
+# don't pollute network mounts with .DS_Store files
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool "true"
+# don't pollute USB mounts with .DS_Store files
+  defaults write com.apple.desktopservices DSDontWriteUSBStores -bool "true"
+  killall Finder
+  killall Dock
+}
