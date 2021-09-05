8d1500f7bb5e9bc01379677ff997142931272f7b
R. Aidan Campbell <aidan.campbell@gmail.com>
2021-09-05 09:14:46 -0700

    fix bug where GOPATH was malformed because it doesn't respect ~

--- .zsh/modules/110.go.sh
+++ .zsh/modules/110.go.sh
@@ -4,4 +4,6 @@ fi
 
 export GOPROXY=https://proxy.golang.org
 export PATH="$HOME/go/bin:$PATH"
-export GOPATH="~/go"
+# can't use `~` in the GOPATH, so we need to evaluate the value, then export it
+_gopath_str="/Users/$(whoami)/go"
+export GOPATH=$_gopath_str
