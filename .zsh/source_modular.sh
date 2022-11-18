a328f2f41b6f3ae1535cb8c2f3080bc420979c92
R. Aidan Campbell <aidan.campbell@gmail.com>
2022-11-18 15:13:04 -0700

    add commented code for showing per-module startup time

--- .zsh/source_modular.sh
+++ .zsh/source_modular.sh
@@ -1,3 +1,8 @@
 for FN in ~/.zsh/modules/* ; do
+#  to enable more human-readable per-module times, uncomment the below
+#    timer=$(($(gdate +%s%N)/1000000))
     source "$FN"
+#    now=$(($(gdate +%s%N)/1000000))
+#    elapsed=$(($now-$timer))
+#    echo $elapsed":" $FN
 done
