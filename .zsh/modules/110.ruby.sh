5ad31287621d2676149481c0022b1ad7aa31d824
R. Aidan Campbell <aidan.campbell@gmail.com>
2022-11-18 15:11:27 -0700

    don't rehash all ruby binaries each time.  This lowers startup time from 210ms to 170ms

--- .zsh/modules/110.ruby.sh
+++ .zsh/modules/110.ruby.sh
@@ -5,7 +5,7 @@ fi
 
 # if rbenv, then do the rbenv do
 if [ -n "$(command -v rbenv)" ] ; then
-    eval "$(rbenv init -)"
+    eval "$(rbenv init - --no-rehash)"
 fi
 export PATH="/usr/local/opt/ruby/bin:$PATH"
 export PATH="$HOME/.rvm/bin:$PATH" # Add RVM to PATH for scripting
